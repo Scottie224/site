f19a1a8f0f059327514038f20c95ca688024538b
Scottie224 <scottie224@hotmail.co.uk>
2014-05-16 12:25:52 +0100

    CSS added

---
+++ Bootstrap/_vti_cnf/Gruntfile.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|15 May 2014 13:46:12 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|SIMON-HP\\Simon
+vti_modifiedby:SR|SIMON-HP\\Simon
+vti_timecreated:TR|15 May 2014 13:46:12 -0000
+vti_cacheddtm:TX|15 May 2014 13:46:12 -0000
+vti_filesize:IR|11328
+vti_backlinkinfo:VX|
---
+++ _vti_cnf/cordova.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|01 Mar 2013 11:16:12 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_author:SR|SIMON-HP\\Simon
+vti_modifiedby:SR|SIMON-HP\\Simon
+vti_timecreated:TR|01 Mar 2013 11:16:12 -0000
+vti_cacheddtm:TX|01 Mar 2013 11:16:12 -0000
+vti_filesize:IR|200664
+vti_backlinkinfo:VX|
