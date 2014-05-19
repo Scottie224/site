9f0c96f0290edc4ebebd0f5c56bbe4f71307d8a9
Scottie224 <scottie224@hotmail.co.uk>
2014-05-19 16:57:14 +0100

    Test External Browser

--- _vti_cnf/cordova.js
+++ _vti_cnf/cordova.js
@@ -6,4 +6,4 @@ vti_modifiedby:SR|SIMON-HP\\Simon
 vti_timecreated:TR|01 Mar 2013 11:16:12 -0000
 vti_cacheddtm:TX|01 Mar 2013 11:16:12 -0000
 vti_filesize:IR|200664
-vti_backlinkinfo:VX|
+vti_backlinkinfo:VX|Site.dwt TrailWebsites.html Trails/Chester.html Trails/Flintshire.html Trails/RibbleValley.html Trails/WaveneyValley.html AboutFoodTrails.html AboutUs.html Help.html Trails/Dengie.html Trails/Exeter.html Trails/Rutland.html index.html Map.html Trails.html Trails/CarmarthenshireRegions.html Trails/ClwydianRange.html
