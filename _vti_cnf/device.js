e6769697c4ad5699e79e1875cd63f705d8f65948
Scottie224 <scottie224@hotmail.co.uk>
2014-05-20 11:08:48 +0100

    Test

---
+++ _vti_cnf/device.js
@@ -0,0 +1,9 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|20 May 2014 09:54:36 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_author:SR|SIMON-HP\\Simon
+vti_modifiedby:SR|SIMON-HP\\Simon
+vti_timecreated:TR|20 May 2014 09:54:36 -0000
+vti_cacheddtm:TX|20 May 2014 09:54:36 -0000
+vti_filesize:IR|2737
