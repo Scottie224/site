83fa783cdb48440cd777cd906d3cf953fc5c3b84
Scottie224 <scottie224@hotmail.co.uk>
2014-05-23 11:32:22 +0100

    Dynamic Map

---
+++ Scripts/Map.js
@@ -0,0 +1,25 @@
+﻿function initialize() {
+
+    //create a variable to hold location (latitude and longitude)
+    var UoCLatlng = new google.maps.LatLng(53.199269, -2.898299);
+
+    //set the map's options (zoom level, centre, type)
+    var mapOptions = {
+        zoom: 11,
+        center: UoCLatlng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    }
+
+    //identify the element to place the map in (e.g. a div)
+    var map = new google.maps.Map(document.getElementById('Map'), mapOptions);
+
+    //create a marker for the map with location and title
+    var marker = new google.maps.Marker({
+        position: UoCLatlng,
+        map: map,
+        title: 'University of Chester'
+    });
+}
+
+//code to run the above initialise code once the window has loaded
+google.maps.event.addDomListener(window, 'load', initialize);
---
+++ Scripts/_vti_cnf/Map.js
@@ -0,0 +1,10 @@
+vti_encoding:SR|utf8-nl
+vti_timelastmodified:TR|23 May 2014 10:06:36 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|Site.dwt TrailWebsites.html Trails/Chester.html Trails/Flintshire.html Trails/RibbleValley.html Trails/WaveneyValley.html AboutFoodTrails.html AboutUs.html Help.html Trails/Dengie.html Trails/Exeter.html Trails/Rutland.html index.html Map.html Trails.html Trails/CarmarthenshireRegions.html Trails/ClwydianRange.html
+vti_author:SR|SIMON-HP\\Simon
+vti_modifiedby:SR|SIMON-HP\\Simon
+vti_timecreated:TR|29 Jan 2014 11:50:38 -0000
+vti_nexttolasttimemodified:TW|29 Jan 2014 11:50:38 -0000
+vti_cacheddtm:TX|23 May 2014 10:06:36 -0000
+vti_filesize:IR|829
