80ade3ceafec47f64df597ba7e631168faa3b303
Scottie224 <scottie224@hotmail.co.uk>
2014-06-02 12:04:43 +0100

    Geolocation

--- Scripts/_vti_cnf/code.js
+++ Scripts/_vti_cnf/code.js
@@ -1,4 +1,9 @@
 vti_encoding:SR|utf8-nl
 vti_timelastmodified:TR|02 Jun 2014 10:56:12 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|Site.dwt TrailWebsites.html Trails/Chester.html Trails/Flintshire.html Trails/RibbleValley.html Trails/Waveney\\ -\\ Beccles\\ and\\ Loddon.html Trails/Waveney\\ -\\ Halesworth\\ &\\ Blyth\\ Valley.html Trails/WorcestershireRegions.html AboutFoodTrails.html AboutUs.html Help.html Trails/Exeter.html Trails/Rutland.html Trails/WaveneyRegions.html Trails/WorcestershireEatingOut.html index.html Map.html Trails.html Trails/CarmarthenshireRegions.html Trails/ClwydianRange.html
+vti_backlinkinfo:VX|Map.html
+vti_author:SR|SIMON-HP\\Simon
+vti_modifiedby:SR|SIMON-HP\\Simon
+vti_timecreated:TR|02 Jun 2014 10:56:12 -0000
+vti_cacheddtm:TX|02 Jun 2014 10:56:12 -0000
+vti_filesize:IR|773
