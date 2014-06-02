091e2f7b51359ee4c28a3138a6168bc19199be99
Scottie224 <scottie224@hotmail.co.uk>
2014-06-02 10:04:03 +0100

    Map Geolocation Test

---
+++ Scripts/geolocation.js
@@ -0,0 +1,25 @@
+﻿function geolocation(){
+//checks geolocation availability
+   var options = { enableHighAccuracy: true };
+   watchID = navigator.geolocation.watchPosition(onSuccess, onError, options);
+   
+//runs if geolocation available
+   function onSuccess(position){
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      
+      localStorage.setItem("latitude", lat);
+      localStorage.setItem("longitude", lng);
+      
+      alert("lat: " +  lat + "long: " + lng);  
+   }
+   
+//runs at error   
+   function onError(error){
+      alert("message: " + error.message);
+   
+   }
+   
+}
+
+geolocation();
