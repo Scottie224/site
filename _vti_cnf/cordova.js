a8cd73e1c4e705694892f697dc41a655f1069b4d
Scottie224 <scottie224@hotmail.co.uk>
2014-06-03 10:04:32 +0100

    Vital images only and home image test html

--- _vti_cnf/cordova.js
+++ _vti_cnf/cordova.js
@@ -6,4 +6,4 @@ vti_modifiedby:SR|SIMON-HP\\Simon
 vti_timecreated:TR|01 Mar 2013 11:16:12 -0000
 vti_cacheddtm:TX|01 Mar 2013 11:16:12 -0000
 vti_filesize:IR|200664
-vti_backlinkinfo:VX|Site.dwt Trails/Chester.html Trails/Flintshire.html Trails/RibbleValley.html Trails/Scotland/MaltWhisky.html Trails/Waveney\\ -\\ Beccles\\ and\\ Loddon.html Trails/Waveney\\ -\\ Halesworth\\ &\\ Blyth\\ Valley.html AboutFoodTrails.html AboutUs.html Help.html Trails/WaveneyRegions.html Trails/Exeter.html Trails/Rutland.html Trails/CarmarthenshireEast.html Trails/CarmarthenshireWest.html Trails/Scotland/FinestFlavours.html Trails/WorcestershireEatingOut.html index.html Map.html Trails.html Trails/CarmarthenshireRegions.html Trails/ClwydianRange.html Trails/CarmarthenshireNorth.html Trails/Scotland/Cheese.html TrailWebsites.html Trails/Scotland/LochLomond.html Trails/Scotland/OuterHebrides.html Trails/Scotland/WhiskyCoast.html Trails/Scotland/RealAle.html Trails/WorcestershireRegions.html
+vti_backlinkinfo:VX|Site.dwt TrailWebsites.html AboutFoodTrails.html AboutUs.html Help.html index.html Map.html Trails.html
--- _vti_cnf/device.js
+++ _vti_cnf/device.js
@@ -1,10 +1,9 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TW|20 May 2014 10:49:23 -0000
+vti_timelastmodified:TR|20 May 2014 10:49:23 -0000
+vti_extenderversion:SR|12.0.0.0
 vti_author:SR|SIMON-HP\\Simon
 vti_modifiedby:SR|SIMON-HP\\Simon
-vti_nexttolasttimemodified:TW|20 May 2014 10:49:23 -0000
 vti_timecreated:TR|20 May 2014 10:49:23 -0000
 vti_cacheddtm:TX|20 May 2014 10:49:23 -0000
 vti_filesize:IR|2737
-vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|Site.dwt Trails/Chester.html Trails/Flintshire.html Trails/RibbleValley.html Trails/Scotland/MaltWhisky.html Trails/Waveney\\ -\\ Beccles\\ and\\ Loddon.html Trails/Waveney\\ -\\ Halesworth\\ &\\ Blyth\\ Valley.html AboutFoodTrails.html AboutUs.html Help.html Trails/WaveneyRegions.html Trails/Exeter.html Trails/Rutland.html Trails/CarmarthenshireEast.html Trails/CarmarthenshireWest.html Trails/Scotland/FinestFlavours.html Trails/WorcestershireEatingOut.html index.html Map.html Trails.html Trails/CarmarthenshireRegions.html Trails/ClwydianRange.html Trails/CarmarthenshireNorth.html Trails/Scotland/Cheese.html TrailWebsites.html Trails/Scotland/LochLomond.html Trails/Scotland/OuterHebrides.html Trails/Scotland/WhiskyCoast.html Trails/Scotland/RealAle.html Trails/WorcestershireRegions.html
+vti_backlinkinfo:VX|Site.dwt TrailWebsites.html AboutFoodTrails.html AboutUs.html Help.html index.html Map.html Trails.html
