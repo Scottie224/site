9487ad352fcce40de72c036871d992d89ab687b6
Scottie224 <scottie224@hotmail.co.uk>
2014-06-03 15:18:07 +0100

    Geolocation test

--- Scripts/_vti_cnf/Map.js
+++ Scripts/_vti_cnf/Map.js
@@ -1,10 +1,10 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|02 Jun 2014 13:49:28 -0000
+vti_timelastmodified:TR|03 Jun 2014 14:00:17 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|Site.dwt Trails/Chester.html Trails/Flintshire.html Trails/RibbleValley.html Trails/Scotland/MaltWhisky.html Trails/Waveney\\ -\\ Beccles\\ and\\ Loddon.html Trails/Waveney\\ -\\ Halesworth\\ &\\ Blyth\\ Valley.html AboutFoodTrails.html AboutUs.html Help.html Trails/WaveneyRegions.html Trails/Exeter.html Trails/Rutland.html Trails/CarmarthenshireEast.html Trails/CarmarthenshireWest.html Trails/Scotland/FinestFlavours.html Trails/WorcestershireEatingOut.html index.html Map.html Trails.html Trails/CarmarthenshireRegions.html Trails/ClwydianRange.html Trails/CarmarthenshireNorth.html Trails/Scotland/Cheese.html TrailWebsites.html Trails/Scotland/LochLomond.html Trails/Scotland/OuterHebrides.html Trails/Scotland/WhiskyCoast.html Trails/Scotland/RealAle.html Trails/WorcestershireRegions.html
+vti_backlinkinfo:VX|Trails/Chester.html Trails/Flintshire.html Trails/RibbleValley.html Trails/Scotland/MaltWhisky.html Trails/Waveney\\ -\\ Beccles\\ and\\ Loddon.html Trails/Waveney\\ -\\ Halesworth\\ &\\ Blyth\\ Valley.html Trails/Scotland/RealAle.html Trails/WorcestershireRegions.html Trails/WorcestershireEatingOut.html Trails/Scotland/FinestFlavours.html Trails/CarmarthenshireWest.html Trails/WaveneyRegions.html Trails/Exeter.html Trails/Rutland.html Trails/CarmarthenshireEast.html Trails/Scotland/Cheese.html Trails/ClwydianRange.html Trails/CarmarthenshireNorth.html Trails/CarmarthenshireRegions.html Map.html Trails/Scotland/WhiskyCoast.html Trails/Scotland/LochLomond.html Trails/Scotland/OuterHebrides.html
 vti_author:SR|SIMON-HP\\Simon
 vti_modifiedby:SR|SIMON-HP\\Simon
 vti_timecreated:TR|29 Jan 2014 11:50:38 -0000
 vti_nexttolasttimemodified:TW|02 Jun 2014 10:54:23 -0000
-vti_cacheddtm:TX|02 Jun 2014 10:54:23 -0000
-vti_filesize:IR|13621
+vti_cacheddtm:TX|03 Jun 2014 14:00:17 -0000
+vti_filesize:IR|16165
