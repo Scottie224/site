0a8589669eec1f615242decb64e09b05b23949fc
Scottie224 <scottie224@hotmail.co.uk>
2014-06-04 01:41:41 +0100

    Fixed trail layouts (no indents)

--- Bootstrap/js/_vti_cnf/collapse.js
+++ Bootstrap/js/_vti_cnf/collapse.js
@@ -1,9 +1,9 @@
 vti_encoding:SR|utf8-nl
-vti_timelastmodified:TR|13 May 2014 09:51:17 -0000
+vti_timelastmodified:TR|15 May 2014 13:46:44 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|Site.dwt Trails/RibbleValley.html Trails/Flintshire.html Trails/Scotland/MaltWhisky.html Trails/Chester.html Trails/Waveney\\ -\\ Beccles\\ and\\ Loddon.html Trails/Waveney\\ -\\ Halesworth\\ &\\ Blyth\\ Valley.html Bootstrap/js/tests/index.html Help.html AboutUs.html AboutFoodTrails.html Trails/Rutland.html Trails/WaveneyRegions.html Trails/Exeter.html Trails/Scotland/FinestFlavours.html Trails/WorcestershireEatingOut.html default.html Trails.html Map.html Trails/ClwydianRange.html Trails/CarmarthenshireRegions.html Trails/Scotland/Cheese.html index.html TrailWebsites.html Trails/Scotland/WhiskyCoast.html Trails/Scotland/LochLomond.html Trails/Scotland/OuterHebrides.html Trails/Scotland/RealAle.html Trails/WorcestershireRegions.html
+vti_backlinkinfo:VX|Site.dwt Trails/RibbleValley.html Trails/Flintshire.html Trails/Scotland/MaltWhisky.html Trails/Chester.html Trails/Waveney\\ -\\ Beccles\\ and\\ Loddon.html Trails/Waveney\\ -\\ Halesworth\\ &\\ Blyth\\ Valley.html Bootstrap/js/tests/index.html Help.html AboutUs.html AboutFoodTrails.html Trails/Rutland.html Trails/WaveneyRegions.html Trails/Exeter.html Trails/Scotland/FinestFlavours.html default.html Trails.html Map.html Trails/ClwydianRange.html Trails/CarmarthenshireRegions.html Trails/Scotland/Cheese.html index.html TrailWebsites.html Trails/Scotland/WhiskyCoast.html Trails/Scotland/LochLomond.html Trails/Scotland/OuterHebrides.html Trails/Scotland/RealAle.html Trails/WorcestershireRegions.html
 vti_author:SR|NOTAVIRUS\\Andy
-vti_modifiedby:SR|NOTAVIRUS\\Andy
+vti_modifiedby:SR|SIMON-HP\\Simon
 vti_timecreated:TR|13 May 2014 09:51:17 -0000
-vti_cacheddtm:TX|13 May 2014 09:51:17 -0000
+vti_cacheddtm:TX|15 May 2014 13:46:44 -0000
 vti_filesize:IR|4840
--- Scripts/_vti_cnf/Map.js
+++ Scripts/_vti_cnf/Map.js
@@ -1,7 +1,7 @@
 vti_encoding:SR|utf8-nl
 vti_timelastmodified:TR|03 Jun 2014 14:16:32 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|Trails/Chester.html Trails/Flintshire.html Trails/RibbleValley.html Trails/Scotland/MaltWhisky.html Trails/Waveney\\ -\\ Beccles\\ and\\ Loddon.html Trails/Waveney\\ -\\ Halesworth\\ &\\ Blyth\\ Valley.html Trails/Scotland/RealAle.html Trails/WorcestershireRegions.html Trails/WorcestershireEatingOut.html Trails/Scotland/FinestFlavours.html Trails/CarmarthenshireWest.html Trails/WaveneyRegions.html Trails/Exeter.html Trails/Rutland.html Trails/CarmarthenshireEast.html Trails/Scotland/Cheese.html Trails/ClwydianRange.html Trails/CarmarthenshireNorth.html Trails/CarmarthenshireRegions.html Map.html Trails/Scotland/WhiskyCoast.html Trails/Scotland/LochLomond.html Trails/Scotland/OuterHebrides.html
+vti_backlinkinfo:VX|Trails/Scotland/WhiskyCoast.html Trails/Scotland/LochLomond.html Trails/Scotland/OuterHebrides.html Trails/WorcestershireRegions.html Trails/Flintshire.html Trails/Scotland/RealAle.html Trails/Scotland/MaltWhisky.html Trails/Waveney\\ -\\ Beccles\\ and\\ Loddon.html Trails/Waveney\\ -\\ Halesworth\\ &\\ Blyth\\ Valley.html Trails/Rutland.html Trails/Scotland/FinestFlavours.html Trails/CarmarthenshireWest.html Trails/WaveneyRegions.html Trails/Exeter.html Trails/Scotland/Cheese.html Map.html
 vti_author:SR|SIMON-HP\\Simon
 vti_modifiedby:SR|SIMON-HP\\Simon
 vti_timecreated:TR|29 Jan 2014 11:50:38 -0000
