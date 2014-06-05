ccde2081effdfb0b5dd6e8bc675d9ab205232867
Scottie224 <scottie224@hotmail.co.uk>
2014-06-05 12:39:06 +0100

    Near final build

--- _vti_cnf/cordova.js
+++ _vti_cnf/cordova.js
@@ -6,4 +6,4 @@ vti_modifiedby:SR|SIMON-HP\\Simon
 vti_timecreated:TR|01 Mar 2013 11:16:12 -0000
 vti_cacheddtm:TX|01 Mar 2013 11:16:12 -0000
 vti_filesize:IR|200664
-vti_backlinkinfo:VX|Site.dwt TrailWebsites.html AboutFoodTrails.html AboutUs.html Help.html index.html Map.html Trails.html
+vti_backlinkinfo:VX|Site.dwt TrailWebsites.html Trails/Scotland/LochLomond.html Trails/WorcestershireFarm.html Trails/WorcestershireCondiments.html Trails/WorcestershireDelicatessens.html Trails/WorcestershireBeverages.html AboutFoodTrails.html AboutUs.html Help.html index.html Map.html Trails.html Trails/WorcestershireBaked.html Trails/WorcestershireMeat.html
--- _vti_cnf/device.js
+++ _vti_cnf/device.js
@@ -6,4 +6,4 @@ vti_modifiedby:SR|SIMON-HP\\Simon
 vti_timecreated:TR|20 May 2014 10:49:23 -0000
 vti_cacheddtm:TX|20 May 2014 10:49:23 -0000
 vti_filesize:IR|2737
-vti_backlinkinfo:VX|Site.dwt TrailWebsites.html AboutFoodTrails.html AboutUs.html Help.html index.html Map.html Trails.html
+vti_backlinkinfo:VX|Site.dwt TrailWebsites.html Trails/Scotland/LochLomond.html Trails/WorcestershireFarm.html Trails/WorcestershireCondiments.html Trails/WorcestershireDelicatessens.html Trails/WorcestershireBeverages.html AboutFoodTrails.html AboutUs.html Help.html index.html Map.html Trails.html Trails/WorcestershireBaked.html Trails/WorcestershireMeat.html
